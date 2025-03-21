974862f4d24dd3b75e883876f9a56e7192947aa7
Ian DeLoach <iandeloach398@gmail.com>
2025-03-21 14:17:11 -0500

    Add buttons to add or delete numbers to answer line

--- src/GUI/CalculatorFrame.java
+++ src/GUI/CalculatorFrame.java
@@ -1,8 +1,25 @@
 package GUI;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Objects;
 
-public class CalculatorFrame  extends JFrame {
+public class CalculatorFrame extends JFrame implements ActionListener {
+
+    JLabel answerLine = new JLabel("0");
+    private JButton btn1 = new JButton("1");
+    private JButton btn2 = new JButton("2");
+    private JButton btn3 = new JButton("3");
+    private JButton btn4 = new JButton("4");
+    private JButton btn5 = new JButton("5");
+    private JButton btn6 = new JButton("6");
+    private JButton btn7 = new JButton("7");
+    private JButton btn8 = new JButton("8");
+    private JButton btn9 = new JButton("9");
+    private JButton btn0 = new JButton("0");
+    private JButton btnBack = new JButton("B");
+    private JButton btnDecimal = new JButton(".");
 
     public CalculatorFrame() {
         this.setTitle("Calculator");
@@ -21,6 +38,9 @@ public class CalculatorFrame  extends JFrame {
 
     public JPanel createAnswerPanel() {
         JPanel panel = new JPanel();
+        answerLine.setHorizontalTextPosition(SwingConstants.RIGHT);
+        answerLine.setForeground(Color.WHITE);
+        panel.add(answerLine);
         panel.setBackground(Color.BLACK);
         panel.setBounds(4, 4, 276, 50);
         return panel;
@@ -29,18 +49,56 @@ public class CalculatorFrame  extends JFrame {
     public JPanel createButtonPanel() {
         JPanel panel = new JPanel();
         panel.setBackground(Color.GRAY);
-        panel.setLayout(new GridLayout(3, 3));
+        panel.setLayout(new GridLayout(4, 3));
         panel.setBounds(4, 60, 276, 396);
-        panel.add(new JButton("1"));
-        panel.add(new JButton("2"));
-        panel.add(new JButton("3"));
-        panel.add(new JButton("4"));
-        panel.add(new JButton("5"));
-        panel.add(new JButton("6"));
-        panel.add(new JButton("7"));
-        panel.add(new JButton("8"));
-        panel.add(new JButton("9"));
+        btn1.addActionListener(this);
+        btn2.addActionListener(this);
+        btn3.addActionListener(this);
+        btn4.addActionListener(this);
+        btn5.addActionListener(this);
+        btn6.addActionListener(this);
+        btn7.addActionListener(this);
+        btn8.addActionListener(this);
+        btn9.addActionListener(this);
+        btn0.addActionListener(this);
+        btnBack.addActionListener(this);
+        btnDecimal.addActionListener(this);
+
+        panel.add(btn1);
+        panel.add(btn2);
+        panel.add(btn3);
+        panel.add(btn4);
+        panel.add(btn5);
+        panel.add(btn6);
+        panel.add(btn7);
+        panel.add(btn8);
+        panel.add(btn9);
+        panel.add(btnBack);
+        panel.add(btn0);
+        panel.add(btnDecimal);
+
         return panel;
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        if ((Objects.equals(e.getActionCommand(), "B"))
+                        && answerLine.getText().length() == 1) {
+            answerLine.setText("0");
+        }
+
+        else if (Objects.equals(e.getActionCommand(), "B")) {
+            answerLine.setText(answerLine.getText().substring(0, answerLine.getText().length() - 1));
+        }
+
+        else if (!Objects.equals(answerLine.getText(), "0")) {
+            answerLine.setText(answerLine.getText() + e.getActionCommand());
+        }
+
+        else {answerLine.setText(e.getActionCommand());}
+
+        //TODO remove out print
+        System.out.println(e.getActionCommand());
+    }
 }
